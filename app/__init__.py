0a26584205f399fae87d3440efa0c2e079b27186
Benie <davinci.monalissa@gmail.com>
2022-05-03 07:36:30 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from .config import DevConfig
 
 # initialize app
 app = Flask(__name__)
 
 # set up config
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 
 from app import views
