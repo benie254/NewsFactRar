a5a4430274fc403db935143e8f9f64636d8a3116
Benie <davinci.monalissa@gmail.com>
2022-05-03 07:37:50 +0300

    Update config.

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from .config import DevConfig
 
 # initialize app
-app = Flask(__name__)
+app = Flask(__name__,instance_relative_config = True)
 
 # set up config
 app.config.from_object(DevConfig)
