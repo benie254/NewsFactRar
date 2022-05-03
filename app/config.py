206fb155387f26ddefa1d5fc49b425c55c68d85a
Benie <davinci.monalissa@gmail.com>
2022-05-03 07:26:58 +0300

    Add configurations.

---
+++ app/config.py
@@ -0,0 +1,22 @@
+class Config:
+    """
+    General configuration--parent class
+    """
+
+    pass
+
+
+class ProdConfig(Config):
+    """
+    Production configuration--child class
+    """
+
+    pass
+
+
+class DevConfig(Config):
+    """
+    Development configuration--child class
+    """
+
+    DEBUG = True
