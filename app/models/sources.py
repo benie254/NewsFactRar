5b2ca612d9cc12374b7d64a1af9062efbd720a7d
Benie <davinci.monalissa@gmail.com>
2022-05-03 08:37:53 +0300

    Update the sources class with parameters for sources objects.

---
+++ app/models/sources.py
@@ -0,0 +1,9 @@
+class Sources:
+    """
+    Define Sources objects
+    """
+
+    def __init__(self,name,description,url):
+        self.name = name
+        self.description = description
+        self.url = url
