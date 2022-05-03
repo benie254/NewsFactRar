a80e2812ca83a8f8fcaf1c9fe42bc1953c5909bc
Benie <davinci.monalissa@gmail.com>
2022-05-03 08:01:24 +0300

    Import urllib.request module for api connections & json module to format api response. && call api key & base urls.

--- app/requests.py
+++ app/requests.py
@@ -1,4 +1,16 @@
 from app import app
+import urllib.request,json
+from .models import articles
+from .models import sources
+
+Articles = articles.Articles
+Sources = sources.Sources
 
 # get api key
 api_key = app.config['NEWS_API_KEY']
+
+# get news base url
+base_url = app.config['NEWS_API_BASE_URL']
+
+# get articles base url
+base_url_art = app.config['ARTICLES_API_BASE_URL']
