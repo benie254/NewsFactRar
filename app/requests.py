fcbdcd21ca5dfa97e6ea37e6382322b79133ae59
Benie <davinci.monalissa@gmail.com>
2022-05-03 08:43:23 +0300

    Install newsapi from pip and import it in the app requests.

--- app/requests.py
+++ app/requests.py
@@ -2,6 +2,7 @@ from app import app
 import urllib.request,json
 from .models import articles
 from .models import sources
+from newsapi import NewsApiClient
 
 Articles = articles.Articles
 Sources = sources.Sources
@@ -14,3 +15,8 @@ base_url = app.config['NEWS_API_BASE_URL']
 
 # get articles base url
 base_url_art = app.config['ARTICLES_API_BASE_URL']
+
+
+def get_headlines():
+
+    newsapi = NewsApiClient(api_key=api_key)
