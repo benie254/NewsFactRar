06a54ccfbcb8611ffdc8107e69b504c1b6e40dc7
Benie <davinci.monalissa@gmail.com>
2022-05-03 08:52:09 +0300

    Create a function to get top headlines.

--- app/requests.py
+++ app/requests.py
@@ -16,7 +16,44 @@ base_url = app.config['NEWS_API_BASE_URL']
 # get articles base url
 base_url_art = app.config['ARTICLES_API_BASE_URL']
 
+newsapi = NewsApiClient(api_key=api_key)
+
 
 def get_headlines():
+    """
+    :return: response to NewsApiClient request
+    """
 
-    newsapi = NewsApiClient(api_key=api_key)
+    get_headlines = newsapi.get_top_headlines(language='en', sources='bbc-news, cnn, reuters, cnbc, techcrunch, the-verge, gizmodo, the-next-web, techradar, recode, ars-technica')
+
+    all_headlines = get_headlines['articles']
+
+    headlines_results = []
+
+    source = []
+    author = []
+    title = []
+    description = []
+    url = []
+    urlToImage = []
+    publishedAt = []
+    content = []
+
+    for headline_item in range(len(all_headlines)):
+        headline = all_headlines[headline_item]
+
+        source.append(headline['source'])
+        author.append(headline['author'])
+        title.append(headline['title'])
+        description.append(headline['description'])
+        url.append(headline['url'])
+        urlToImage.append(headline['urlToImage'])
+        publishedAt.append(headline['publishedAt'])
+        content.append(headline['content'])
+
+        headline_object = Articles(source, author, title, description, url, urlToImage, publishedAt, content)
+        headlines_results.append(headline_object)
+
+        contents = zip(source, author, title, description, url, urlToImage, publishedAt, content)
+
+    return contents
